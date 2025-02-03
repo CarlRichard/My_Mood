1001fbec44b67a84e2a8c1bef469ef51e9b8363d
matias <matias.daligault@institutsolacroup.com>
2025-02-03 11:18:14 +0100

    matias

--- public/js/administration/modif-profil-admin.js
+++ public/js/administration/modif-profil-admin.js
@@ -1,18 +1,35 @@
 // je selection mes éléments nécessaires
-const okButton = document.querySelector ('.modif_button');
-const adminButton = document.querySelector ('.admin-btn');
-const logoutButton = document.querySelector ('.logout-btn');
+const okButton = document.querySelector(".modif_button");
+const adminButton = document.querySelector(".admin-btn");
+const logoutButton = document.querySelector(".logout-btn");
 
 // je crée un évenement au click qui me redirige vers une autre page
 okButton.addEventListener("click", function () {
-    window.location.href = "../../pages/stagiaires/mymood.html"; 
+  window.location.href = "../../pages/stagiaires/mymood.html";
 });
 
 adminButton.addEventListener("click", function () {
-    window.location.href = ""; 
+  window.location.href = "";
 });
 
-document.getElementById('.logout-btn').addEventListener("click", function() {
-    localStorage.removeItem('tokenUser'); // Supprime le token
-    window.location.href = '../../index.html'; // Redirige vers la page de connexion
+logoutButton.addEventListener("click", function () {
+  // Envoi de la requête POST à l'API Symfony pour la déconnexion
+  fetch("/logout", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message === "Déconnexion réussie") {
+        // Effacer le token dans localStorage
+        localStorage.removeItem("token");
+        // Rediriger vers la page de connexion
+        window.location.href = "../../index.html";
+      }
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la déconnexion:", error);
+    });
 });
